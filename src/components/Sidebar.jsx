92ce658c2aaf755ecc6ee6c89f1299f227f2ba7f
Juan Torreal <juantorreal@gmail.com>
2022-06-11 19:53:00 -0300

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
 
@@ -8,11 +9,16 @@ return (
         className={styles.cover}   
         src="https://images.unsplash.com/photo-1599420186946-7b6fb4e297f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"/>
         <div className={styles.profile}>
+            <img className={styles.avatar} src="https://github.com/Juantorreal.png" />
 <strong>Juan Torreal</strong>
 <span>Web Developer</span>
     </div>
     <footer>
-        <a href="#">Editar seu perfil</a>
+       
+        <a href="#">  
+        <PencilLine size={20}/> 
+        Editar seu perfil
+        </a>
     </footer>
     </aside>
 
