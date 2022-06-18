e959aa40ad7944e68b5e4a3c19a321f66c15a15e
Juan Torreal <juantorreal@gmail.com>
2022-06-18 16:57:36 -0300

    map

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,62 @@ import './global.css'
 import styles from './App.module.css';
 import { Sidebar } from "./components/Sidebar";
 
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:"https://github.com/Juantorreal.png",
+      name: 'Juan Torreal',
+      role: 'Web Developer'
+    }, 
+    content: [
+      {type: 'paragraph' ,content: 'Fala galeraa 👋'},
+{type: 'paragraph' ,content:"Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+{type: 'link', content: 'jane.design/doctorcare'},
+
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl:"https://github.com/maykbrito.png",
+      name: 'Mayk brito',
+      role: 'Educator'
+    }, 
+    content: [
+      {type: 'paragraph' ,content: 'Fala galeraa 👋'},
+{type: 'paragraph' ,content:"Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀"},
+{type: 'link', content: 'jane.design/doctorcare'},
+
+    ],
+    publishedAt: new Date('2022-05-07 20:00:00')
+
+  }
+  
+];
+
 export function App() {
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
 <Sidebar />
-<main><Post author="Juan" content="lorem lorem" /> </main>
+<main>
+  {posts.map(post =>{
+ return  (<Post 
+  author={post.author}
+  content={post.content}
+  publishedAt={post.publishedAt}
+
+ 
+ /> 
+ 
+ )
+  })}
+   </main>
       </div>
   
   </div>
